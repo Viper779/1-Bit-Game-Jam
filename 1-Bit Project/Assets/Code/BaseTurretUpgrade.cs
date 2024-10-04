64f8c1117f5f9b651e9f3cff502a43fb96c91865
Viper779 <aaedling@gmail.com>
2024-10-03 19:52:01 -0500

    turret tier movement

--- 1-Bit Project/Assets/Code/BaseTurretUpgrade.cs
+++ 1-Bit Project/Assets/Code/BaseTurretUpgrade.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 public class BaseTurretUpgrade : MonoBehaviour
 {
-    private int currentTier = 0;
-    public float yMove = 0.5f;
+    public int currentTier = 0;
+    public float yMove = 0.34f;
     public float buildSpeed = 0.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.position = new Vector3(16, -4.7f, 0); // Reset to initial position
+        gameObject.transform.position = new Vector3(-16, -4.7f, 0); // Reset to initial position
     }
 
     // Update is called once per frame
