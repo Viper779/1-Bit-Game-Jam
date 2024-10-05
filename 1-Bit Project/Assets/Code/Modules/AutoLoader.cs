6c1fda02cd518f967fb28a99fa038659aa1e6853
Viper779 <aaedling@gmail.com>
2024-10-04 21:03:36 -0500

    Added Bot Factory Logic Started on AutoLoader

---
+++ 1-Bit Project/Assets/Code/Modules/AutoLoader.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class AutoLoader : MonoBehaviour
+{
+    [SerializeField] private float frameRate = 0.1f;
+    [SerializeField] private Sprite[] Dakka;
+    public SpriteRenderer spriteRenderer;
+    private int currentFrame;
+    private float frameTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayAnimation();
+    }
+
+
+    // Plays the frame animation
+    void PlayAnimation()
+    {
+        frameTimer -= Time.deltaTime;
+        if (frameTimer <= 0f)
+        {
+            frameTimer += frameRate;
+            if (currentFrame < Dakka.Length)
+            {
+                spriteRenderer.sprite = Dakka[currentFrame];
+                currentFrame++;
+            }
+            else
+            {
+                currentFrame = 0; // Reset to the beginning of the animation
+                spriteRenderer.sprite = Dakka[currentFrame];
+            }
+        }
+    }
+}
